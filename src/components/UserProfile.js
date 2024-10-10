c6801eb2bc113d36c3d506c0be4ed87bd27c97ff
Pholisa <nentlenofemele@outlook.com>
2024-10-10 09:16:10 +0200

    2nd commit

---
+++ src/components/UserProfile.js
@@ -0,0 +1,25 @@
+import React, {useEffect, useState} from 'react';
+import { useParams } from 'react-router-dom';
+
+function UserProfile() {
+    const {username} = useParams();
+    const [profile, setProfile] = useState(null);
+
+    useEffect(() => {
+
+        const fetchProfile = async () => {
+            setProfile({name: username, bio: 'This is a user bio.'});
+
+        };
+        fetchProfile();
+    }, [username]);
+
+    return (
+        <div>
+            <h1>User Profile: {profile ? profile.name : 'Loading....'}</h1>
+            <p>{profile ? profile.bio :''}</p>
+        </div>
+    );
+}
+
+export default UserProfile;
